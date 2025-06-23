4c5537a727fae0dc8895aaabfc0bbb887700102a
Paridhipatidar03 <patidarparidhi35@gmail.com>
2025-06-23 12:20:53 +0530

    Backend Full Directory Structure added

---
+++ I_A_PBackend/src/main/java/com/practice/IAPBackendApplication.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IAPBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IAPBackendApplication.class, args);
+	}
+
+}
